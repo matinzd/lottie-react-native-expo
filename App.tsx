111692aabb87991b8750091262464cdbd575e5a0
matinzd <zadehdolatabad@gmail.com>
2024-01-28 14:09:39 +0100

    chore: initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import LottieView from "lottie-react-native";
 
-export default function App() {
+export default function Animation() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LottieView
+      source={require("./assets/animation.json")}
+      autoPlay={true}
+      style={{ width: "100%", height: "100%" }}
+      loop={true}
+      speed={2.2}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
